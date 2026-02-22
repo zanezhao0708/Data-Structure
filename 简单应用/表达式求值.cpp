014b10f297ef51f009f2db65431a6b40616b5c28
zanezhao0708 <zeruizhao78@gmail.com>
2026-02-22 12:02:44 +0800

    solution

--- 简单应用/表达式求值.cpp
+++ 简单应用/表达式求值.cpp
@@ -1,7 +1,6 @@
 //前缀表达式（注意是前缀）从右向左扫描，因为从左向右扫描时，无法确定运算符的右操作数是什么
 //后缀表达式从左向右扫描，因为从右向左扫描时，无法确定运算符的左操作数是什么
 
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <cstdlib> 
@@ -13,5 +12,5 @@ typedef struct StackNode{
 
 void solution(){
 
-    
+
 }
