a297fe7cbd7ebd880636cd631d6b573a1edc7827
zanezhao0708 <zeruizhao78@gmail.com>
2026-02-25 19:30:45 +0800

    graph

---
+++ 图/图.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <cstdlib>
+#define MAXSIZE 30
+
+//邻接表
+typedef struct AdjArc
+{
+    int adjvex;//该弧指向的顶点的位置
+    struct AdjArc* next;
+    int info;
+}AdjArc;
+
+typedef struct AdjNode
+{
+    int data;
+    AdjArc* first;
+}AdjNode, AdjList[MAXSIZE];
+
+typedef struct AdjGraph
+{
+    int kind;
+    int arc ,vex;
+    AdjList vexs;
+}AdjGraph;
+
+
+
+
+
+
+
